c44c531d25139afb7dd9739ead3952fc9c3dfb51
Ben Sima <bensima@gmail.com>
2015-03-14 19:03:17 -0400

    Setup some scaffolding

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "src/collect.c";
+
+/**
+ * Main entrypoint
+ *
+ * This will be a CLI program for interacting with the CheapStat data.
+ */
+
+void main() {
+  // FIXME: write the main interface file :)
+} 
---
+++ test.c
@@ -0,0 +1,13 @@
+#include "src/collect.c";
+#include "t/collect.c";
+
+/**
+ * Test all the things!
+ *
+ * Just runs the test files and reports the outcome.
+ */
+
+
+void main() {
+  // FIXME: write this tester file
+}
