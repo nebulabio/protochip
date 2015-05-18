7925646763c75a041daa558cf5b6b44e6d68f2be
Ben Sima <bensima@gmail.com>
2015-05-17 21:19:56 -0400

    Driver now compiles and reads correctly
    Example cheapstat output: https://gist.github.com/55114c099acec75430a4

--- Makefile
+++ Makefile
@@ -6,7 +6,9 @@ driver_dist  = $(dist_path)/driver
 driver_deps := $(foreach dir, driver/deps, $(wildcard $(dir)/*/*.c))
 driver_src   = ./driver/main.c
 
+CC = gcc
 
+# FIXME: when the gui is added to the makefile, put it under dist task too
 dist: driver
 
 
@@ -14,7 +16,7 @@ dist_dir:
 	@mkdir -p $(dist_path)
 
 driver: dist_dir
-	$(CC) -std=c99 -Wall $(driver_src) $(driver_deps) -o $(driver_dist)
+	$(CC) -std=c99 -g -Wall $(driver_src) $(driver_deps) -o $(driver_dist)
 
 clean:
 	rm -rf $(dist_path)
--- driver/main.c
+++ driver/main.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <stdint.h>
 
-#include <termios.h>
 #include <errno.h>
 #include <unistd.h> 
 
@@ -32,8 +31,6 @@
  * A good program to take inspiration from is mon:
  * https://github.com/tj/mon/blob/master/src/mon.c
  */ 
-#define CHEAPSTAT "/dev/ttyUSB0" 
-#define BAUDRATE  "B9600"        // baud is 9600, c_cflag = B9600
 
 typedef struct { // legacy struct from the mcu, don't change
 	char name[15];
@@ -57,132 +54,44 @@ typedef struct { // legacy struct from the mcu, don't change
   uint8_t curr_range;
 } swv;
 
+
 // Struct for the driver state
 typedef struct {
   const char *pidfile;
   const char *logfile;
-  char *port;
+  const char *port;
   int   baud;
   int   fd;
   int   daemon;
   int   verbose;
 } driver_t; 
 
-// Initiate state with default values
+
+
+/********************************** 
+  initialize state
+**********************************/
+
 static driver_t driver;
-int verbose = 0;
-//driver.verbose->0;
-//driver.baud   ->9600; 
-//driver.fd     ->-1;
 
 
-// Function signatures
-static void exithandler(driver_t *driver);
-static void print_profile(profile data, int fd);
-static void print_swv(swv data, int fd);
-static void take_ownership(char *port);
 
+/********************************** 
+  helper functions 
+**********************************/
 
-// Public api - FIXME: flesh this out
-//               also: http://lucumr.pocoo.org/2013/8/18/beautiful-native-libraries/
-void list_ports();
-void runn(const char *cmd, driver_t *driver);
+static void exithandler()    { printf("Closing the connection.\n"); serialClose(driver.fd); }
+static void error(char *msg) { fprintf(stderr, "Error: %s\n", msg); exit(1); }
 
-
-// entrypoint helpers
-static void set_verbose(command_t *self) { verbose = 1; };
-static void error(char *msg)             { fprintf(stderr, "Error: %s\n", msg); exit(1); }
-static void tbd(command_t *self)         { printf("to be developed!"); exit(1); } 
-
-// entrypoint
-int main(int argc, char **argv) {
-  command_t cli;
-  
-  command_init(&cli, argv[0], "0.0.1");
-  command_option(&cli, "-v", "--verbose", "enable verbose output", set_verbose);
-  command_option(&cli, "-l", "--list", "list all available ports", list_ports);
-  command_option(&cli, "-d", "--daemon", "run as a daemon in the background", tbd);
-  command_option(&cli, "-p", "--port", "specify the port. If not specified, tries to guess based on your platform.", tbd);
-  command_option(&cli, "-b", "--baud", "specify the baud rate, defaults to 9600", tbd);
-  command_parse(&cli, argc, argv);
-
-  printf("additional args:\n");
-  for (int i = 0; i < cli.argc; ++i) {
-    printf("  - '%s'\n", cli.argv[i]);
-  }
-
-  // command required
-  if (!cli.argc) error("<cmd> required");
-  const char *cmd = cli.argv[0];
-
-  runn(cmd, &driver);
-
-  // don't think I need this?
-  //command_free(&driver); 
-  return 0;
-}
-
-
-// actual functionality
-
-static void exithandler(driver_t *driver) { printf("Closing the connection.\n"); serialClose(driver->fd); }
-
-static void print_profile(profile data, int fd) {
-  printf("\n"); // Begin
-  printf("File descriptor is %i\n", fd);
-
-  printf("  Description  \t Data\tAddress\t\tSize\n");
-  printf("---------------------------------------------------\n");
-  printf("         Name:\t%s\t%p\t%lu\n", data.name,       &data.name,       sizeof(data.name));
-  printf("         Type:\t%u\t%p\t%lu\n", data.type,       &data.type,       sizeof(data.type));
-  printf("  Operation 1:\t%d\t%p\t%lu\n", data.op1,        &data.op1,        sizeof(data.op1));
-  printf("  Operation 2:\t%d\t%p\t%lu\n", data.op2,        &data.op2,        sizeof(data.op2));
-  printf("  Operation 3:\t%d\t%p\t%lu\n", data.op3,        &data.op3,        sizeof(data.op3));
-  printf("  Operation 4:\t%d\t%p\t%lu\n", data.op4,        &data.op4,        sizeof(data.op4));
-  printf("  Operation 5:\t%d\t%p\t%lu\n", data.op5,        &data.op5,        sizeof(data.op5));
-  printf("  Operation 6:\t%d\t%p\t%lu\n", data.op6,        &data.op6,        sizeof(data.op6));
-  printf("Current Range:\t%d\t%p\t%lu\n", data.curr_range, &data.curr_range, sizeof(data.curr_range));
-  printf("---------------------------------------------------\n");
-  printf("  Total Bytes: \t %lu\n\n", sizeof(data));
-
-  for (int p = 0; p < sizeof(data.name); p++) {
-    printf("\tname[%i]:\t%x\n", p, data.name[p]);
-  }
-
-  printf("\n"); // End of printer
-}
-
-static void print_swv(swv data, int fd) {
-  printf("\nFile descriptor is %i\n", fd);
-
-  printf("  Description  \t Data\tAddress\t\tSize\n");
-  printf("---------------------------------------------------\n");
-  printf("         Name:\t%s\t%p\t%lu\n", data.name,       &data.name,       sizeof(data.name));
-  printf("    Frequency:\t%u\t%p\t%lu\n", data.freq,       &data.freq,       sizeof(data.freq));
-  printf("        Start:\t%i\t%p\t%lu\n", data.start,      &data.start,      sizeof(data.start));
-  printf("         Stop:\t%d\t%p\t%lu\n", data.stop,       &data.stop,       sizeof(data.stop));
-  printf("       Height:\t%d\t%p\t%lu\n", data.height,     &data.height,     sizeof(data.height));
-  printf("    Increment:\t%d\t%p\t%lu\n", data.increment,  &data.increment,  sizeof(data.increment));
-  printf("Current Range:\t%d\t%p\t%lu\n", data.curr_range, &data.curr_range, sizeof(data.curr_range));
-  printf("---------------------------------------------------\n");
-  printf("  Total Bytes: \t %lu\n\n", sizeof(data));
-
-  for (int p = 0; p < sizeof(data.name); p++) {
-    printf("\tname[%i]:\t%x\n", p, data.name[p]);
-  }
-
-  printf("\n"); // End of printer
-}
-
-
-static void take_ownership(char *port) {
+static void take_ownership(const char *port) {
   int  user = getuid();
   int group = getgid();
 
 #ifdef DEBUG
-  printf("\nUID:\t%d\nGID:\t%d\n", user, group);
-  printf("Port is: %s", port);
+  printf("\nUID:\t%d\nGID:\t%d\n\n", user, group);
 #endif
+
+  printf("Port is: %s\n\n", port);
 
   // int chown(const char *path, uid_t owner, gid_t group);
   if (chown(port, user, group) == -1) { 
@@ -192,22 +101,48 @@ static void take_ownership(char *port) {
 }
 
 
-void list_ports() {
-  char **ports = getSerialPorts();
-  char **tmp = ports;
-  printf("Available serial ports:\n");
 
-  while (*tmp != NULL) { printf(" %s\n", *(tmp++)); }
-  free(ports);
+/********************************** 
+  stateful functions 
+
+  i.e., functions that modify the
+  `driver_t driver` struct.
+**********************************/
+
+void cheapstat_reader(driver_t *driver) {
+  while (driver->fd != -1) {
+    if (serialHasChar(driver->fd)) {
+      uint8_t b;
+      read(driver->fd, &b, 1);
+      printf("%02x", b);
+    }
+  }
 }
 
-// FIXME: refactor this to dispatch on cmd
-// for inspiration: https://github.com/tj/mon/blob/master/src/mon.c#L326
-void runn(const char *cmd, driver_t *driver) {
+// FIXME: write a daemonize function
+//static void daemonize() {}
 
-#ifdef DEBUG
-  printf("running %s", cmd); // yeah, this cmd doesn't do anything, there's only one command
-#endif
+/********************************** 
+  driver processes
+
+  i.e., top-level commands for 
+  the cli
+**********************************/
+
+/*
+ * stops the driver and gracefully shuts down all things
+ * 
+ * FIXME: this segfaults
+ */
+//static void stop(driver_t *driver) {
+//  free(&driver);
+//}
+
+
+/*
+ * starts the driver
+ */ 
+static void start(driver_t *driver) {
 
   take_ownership(driver->port);
 
@@ -220,33 +155,116 @@ void runn(const char *cmd, driver_t *driver) {
 
   atexit(exithandler);
 
+
 #ifdef DEBUG  
   printf("Connected! Datareceived will be printed...\n");
   printf("Use CTRL+C to quit.\n");
 #endif
 
-  while (driver->fd != -1) {
-    if (serialHasChar(driver->fd)) {
 
-      swv d;
-      read(driver->fd, &d, 29);
-      print_swv(d, driver->fd);
-
-      //short int type;
-        
-
-      //printf("Getting results for: %s", type);
-        
-      /*profile p; int *ptr = &p; 
-
-        while (ptr < 29) {
-
-        if ( ptr < 16 ) { p.name = serialReadRaw(fd, ptr, 1); ptr += 14; } 
-        if ( ptr < 32 ) { p.type = serialReadRaw(fd, ptr, 8); ptr  = 29; }
-
-        }*/
-    }
-  }
+  cheapstat_reader(driver);
 }
 
+
+/*
+ * lists available ports
+ */
+void list(driver_t *driver) {
+  char **ports = getSerialPorts();
+  char **tmp = ports;
+  printf("Available serial ports:\n");
+
+  while (*tmp != NULL) { printf(" %s\n", *(tmp++)); }
+  free(ports);
+}
+
+
+
+/********************************** 
+  cli settings
+**********************************/
+
+
+/*
+ * -i, --pidfile <path>
+ */
+static void set_pidfile(command_t *self) {
+  driver_t *driver = (driver_t *) self->data;
+  driver->pidfile = self->arg;
+}
+
+/*
+ * -g, --logfile <path>
+ */
+static void set_logfile(command_t *self) {
+  driver_t *driver = (driver_t *) self->data;
+  driver->logfile = self->arg;
+}
+
+/*
+ * -b, --baud <num>
+ */
+static void set_baud(command_t *self) {
+  driver_t *driver = (driver_t *) self->data;
+  driver->baud = atoi(self->arg);
+}
+
+/*
+ * -p, --port <path>
+ */
+static void set_port(command_t *self) {
+  driver_t *driver = (driver_t *) self->data;
+  driver->port = self->arg;
+}
+
+/*
+ * -v, --verbose
+ */
+static void set_verbose(command_t *self) {
+  driver_t *driver = (driver_t *) self->data;
+  driver->verbose = 1;
+}
+
+/*
+ * -d, --daemon
+ */
+static void set_daemon(command_t *self) {
+  driver_t *driver = (driver_t *) self->data;
+  driver->daemon = 1;
+}
+
+
+// entrypoint
+int main(int argc, char **argv) {
+  command_t cli;
+
+  driver.pidfile = ""; //FIXME
+  driver.logfile = ""; //FIXME
+  driver.port    = "/dev/ttyUSB0"; //FIXME
+  driver.baud    = 9600;
+  driver.fd      = -1;
+  driver.daemon  = 0;
+  driver.verbose = 0;
+  
+
+  command_init(&cli, argv[0], "0.0.1");
+  command_option(&cli, "-i", "--pidfile <path>", "specify the pidfile location", set_pidfile); // FIXME: remove this?
+  command_option(&cli, "-g", "--logfile <path>", "specify the logfile location", set_logfile);
+  command_option(&cli, "-b", "--baud <num>", "specify the baud rate, defaults to 9600", set_baud);
+  command_option(&cli, "-p", "--port <path>", "specify the port. If empty, tries to guess based on your platform", set_port);
+  command_option(&cli, "-v", "--verbose", "enable verbose output", set_verbose);
+  command_option(&cli, "-d", "--daemon", "run as a daemon in the background", set_daemon);
+
+ 
+  command_parse(&cli, argc, argv);
+
+  if (!cli.argc) error("<cmd> required");
+
+  if (strcmp(cli.argv[0], "start") == 0) { start(&driver); } 
+  if (strcmp(cli.argv[0], "list") == 0)  { list(&driver);  }
+
+  command_free(&cli);
+
+  return 0;
+}
 
