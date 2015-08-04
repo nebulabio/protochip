615e812f3dae2881ab3f76df30b93c9e6ef052e8
Ben Sima <bensima@gmail.com>
2015-08-04 15:45:03 -0400

    add some boilerplate

--- wetware/Test_Results/Linear_Regression.py
+++ wetware/Test_Results/Linear_Regression.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+##
 ## Python Script for performing linear regression
 ## on Stock Cortisol Characterization Results
 ## This script takes optical density results and performs a linear
@@ -10,15 +12,15 @@ from scipy import special, optimize
 from scipy import stats
 
 ## Data
-OD  = np.array([.062,.112,.194,.314,.4,.504,.606,])
-C   = np.array([10,5,2.5,1.25,.625,.313,.156,])
-LC  = np.log10(C)
-#print(OD)
-#print(C)
-#print(LC)
+optical_density = np.array([.062,.112,.194,.314,.4,.504,.606,])
+concentration = np.array([10,5,2.5,1.25,.625,.313,.156,])
+log_concentration = np.log10(concentration)
 
 ## Linear Regression
-slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(LC, OD)
-print(p)
-print(R)
+slope, intercept, r_value, p_value, std_err = stats.linregress(log_concentration, optical_density)
 
+# print slope, intercept, and r_value
+if __name__ == '__main__':
+    print("Slope:\t\t{}".format(slope))
+    print("Intercept:\t{}".format(intercept))
+    print("R-value:\t{}".format(r_value))
