e0552052c6ed497f6112a9979b1499d2296a1dc9
Ben Sima <bensima@gmail.com>
2015-08-05 21:16:48 -0400

    Add plotting with -p flag
    A simple plot for the linear regression. Plot the data by adding a -p
    argument when running the script. I also added the argument parser and
    extracted some of the functionality out into functions

--- wetware/Test_Results/Linear_Regression.py
+++ wetware/Test_Results/Linear_Regression.py
@@ -1,26 +1,47 @@
 #!/usr/bin/env python
-##
-## Python Script for performing linear regression
-## on Stock Cortisol Characterization Results
-## This script takes optical density results and performs a linear
-## regression on a logarithmic scale
+#
+# Python Script for performing logistic regression
+# on Stock Cortisol Characterization Results
+#
+# Requires Scipy and numpy
 
-## Requires Scipy and numpy
 
+import argparse
 import numpy as np
-from scipy import special, optimize
-from scipy import stats
+from scipy import special, optimize, stats
+import matplotlib.pyplot as plt
+
 
 ## Data
-optical_density = np.array([.062,.112,.194,.314,.4,.504,.606,])
-concentration = np.array([10,5,2.5,1.25,.625,.313,.156,])
-log_concentration = np.log10(concentration)
+optical_densities = np.array([0.062, 0.112, 0.194, 0.314, 0.4, 0.504, 0.606])
+concentrations = np.array([10.0, 5.0, 2.5, 1.25, 0.625, 0.313, 0.156])
+log_concentrations = np.log10(concentrations)
+
 
 ## Linear Regression
-slope, intercept, r_value, p_value, std_err = stats.linregress(log_concentration, optical_density)
+slope, intercept, r_value, p_value, std_err = stats.linregress(log_concentrations, optical_densities)
 
-# print slope, intercept, and r_value
-if __name__ == '__main__':
+
+def plot_results():
+    plt.figure()
+    # First add original data
+    plt.plot(optical_densities, concentrations, 'ro', label="Original Data")
+    plt.plot(log_concentrations, 'ko', label="log of concentrations")
+    plt.legend()
+    plt.show()
+
+
+def print_results(slope, intercept, r_value):
     print("Slope:\t\t{}".format(slope))
     print("Intercept:\t{}".format(intercept))
     print("R-value:\t{}".format(r_value))
+
+
+# print slope, intercept, and r_value
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Calculate and optionally plot the regression results.")
+    parser.add_argument('-p', '--plot', help="Toggle the plot (matplotlib)", action='store_true')
+    args = parser.parse_args()
+
+    print_results(slope, intercept, r_value)
+    plot_results() if args.plot == True else None
